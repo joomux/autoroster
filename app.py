cb08de59a4b46c73194a6f5aa1bce899d93c3fcc
Jeremy Roberts <jeremy.e.roberts@gmail.com>
2026-04-01 10:19:03 +1100

    remove duplicate routes

--- app.py
+++ app.py
@@ -126,14 +126,14 @@ def google_site_verification():
     return app.send_static_file("google033ac8bfa05a6917.html")
 
 
-@app.route("/privacy")
-def privacy():
-    return render_template("privacy.html")
+# @app.route("/privacy")
+# def privacy():
+#     return render_template("privacy.html")
 
 
-@app.route("/terms")
-def terms():
-    return render_template("terms.html")
+# @app.route("/terms")
+# def terms():
+#     return render_template("terms.html")
 
 
 @app.route("/logout")
